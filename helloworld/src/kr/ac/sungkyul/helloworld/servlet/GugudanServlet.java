bbfc2dff2c76c4441b6d02226581d22c4dd728d3
S401-07 <S401-07@S401-07>
2016-08-02 15:52:06 +0900

    20160802 웹 실습

---
+++ helloworld/src/kr/ac/sungkyul/helloworld/servlet/GugudanServlet.java
@@ -0,0 +1,33 @@
+package kr.ac.sungkyul.helloworld.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		for (int i = 1; i < 10; i++) {
+			out.println();
+			for (int j = 1; j < 10; j++) {
+				out.println("<pre>" + i + " * " + j + " = " + (i * j) + "</pre>");
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
